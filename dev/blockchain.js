63d230fea08568b9345a8cd2e7b4b3daee4f8178
kojima yosuke <yabbee0828@gmail.com>
2021-09-19 16:23:55 +0900

    Add proof of work functions

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,6 +1,9 @@
+const sha256 = require('sha256');
 const Blockchain = function () {
   this.chain = [];
   this.pendingTransactions = []; // これは言わば、pendingな状態のtransactionと見ていい。また、set in stone状態ではないものとみなせる。
+
+  this.createNewBlock(100, '0', '0'); // genesis blockになる。
 };
 // これが、根本のdata structureになる。
 
@@ -31,6 +34,25 @@ Blockchain.prototype.createNewTransaction = function (amount, sender, recipient)
   };
   this.pendingTransactions.push(newTransaction);
   return this.getLastBlock['index'] + 1; // これにより、作られたtransactionが次に来るべき番号が決まる。
+};
+
+Blockchain.prototype.hashBlock = function (previousBlockHash, currentBlockData, nonce) {
+  const dataAsString = previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
+  const hash = sha256(dataAsString);
+  return hash;
+};
+
+// このmethodがとても根幹になってくる。
+Blockchain.prototype.proofOfWork = function (previousBlockHash, currentBlockData) {
+  let nonce = 0;
+  let hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+  while (hash.substring(0, 4) !== '0000') {
+    // このsubstringの0,4は4を含めないからね。
+    nonce++;
+    hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+  }
+  // これ、かなり計算するね。100,000,000くらいのhash algorithmを実行することも珍しくないだろう。
+  return nonce; // これがproofとなる重要なnumber only used onceになる。
 };
 
 module.exports = Blockchain;
--- dev/test.js
+++ dev/test.js
@@ -1,13 +1,43 @@
 const Blockchain = require('./blockchain');
 
 const bitcoin = new Blockchain();
-bitcoin.createNewBlock(3467, 'F1P9HG498HI', 'DJJO248TQOFHQ');
-bitcoin.createNewTransaction(100, 'JIM_F9N834HHF1O', 'JOHN_DJOHF0HF90U9');
-bitcoin.createNewBlock(34, 'UDHQFG98RQJ', 'CNWOEHFI20I1V'); //ここで新たにminingする。。。
-bitcoin.createNewTransaction(3000, 'MUSK_U81G9GF1', 'BILL_8R1GFN3OF');
-bitcoin.createNewTransaction(9, 'JOBS_JF89H11', 'LARRY_JI2HIHI92F');
-bitcoin.createNewTransaction(772, 'WARREN_JID2O1F3', 'JEFF_U82B1FF1');
-bitcoin.createNewTransaction(45, 'MARK_YUIFBI0U93', 'JACK_I90HIFOFF');
-bitcoin.createNewBlock(7832, 'HF89F2G02G', 'UF8HF22UO'); // miningすることで、取引が完了する仕組みなんだ。逆に、mineされなければ、その取引は完了しない。
+// console.dir(bitcoin, { depth: null });
+// --------    block作って、transactionを行う。 -------------
 
-console.dir(bitcoin, { depth: null });
+// bitcoin.createNewBlock(3467, 'F1P9HG498HI', 'DJJO248TQOFHQ');
+// bitcoin.createNewTransaction(100, 'JIM_F9N834HHF1O', 'JOHN_DJOHF0HF90U9');
+// bitcoin.createNewBlock(34, 'UDHQFG98RQJ', 'CNWOEHFI20I1V'); //ここで新たにminingする。。。
+// bitcoin.createNewTransaction(3000, 'MUSK_U81G9GF1', 'BILL_8R1GFN3OF');
+// bitcoin.createNewTransaction(9, 'JOBS_JF89H11', 'LARRY_JI2HIHI92F');
+// bitcoin.createNewTransaction(772, 'WARREN_JID2O1F3', 'JEFF_U82B1FF1');
+// bitcoin.createNewTransaction(45, 'MARK_YUIFBI0U93', 'JACK_I90HIFOFF');
+// bitcoin.createNewBlock(7832, 'HF89F2G02G', 'UF8HF22UO'); // miningすることで、取引が完了する仕組みなんだ。逆に、mineされなければ、その取引は完了しない。
+
+// console.dir(bitcoin, { depth: null });
+
+// ---------- hashing algorithms , proof of work------------------
+// const previousBlockHash = 'H8F91OBLIR9E3';
+// const currentBlockData = [
+//   {
+//     amount: 23,
+//     sender: 'BOB_J89DOBF',
+//     recipient: 'SAM_J893OIBF',
+//   },
+//   {
+//     amount: 1832,
+//     sender: 'ALICE_NCOQRHVV',
+//     recipient: 'JEFFRY_H89V3BVO',
+//   },
+//   {
+//     amount: 107,
+//     sender: 'CHIRIS_FN81N9OH',
+//     recipient: 'DOE_FJ30131O',
+//   },
+// ];
+
+// //　前のblock hashと、今重なっているtransactionのデータを使って、0000から始まるまあ、hash dateをとにかく作って、最終的にそのnonce（number only used once）が返ってくる。
+
+// console.log(bitcoin.proofOfWork(previousBlockHash, currentBlockData));
+// // →1秒くらいかかって232385っていうnonceが返ってきた。
+// console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, 232385));
+// 結果のnonceを使って、hashを生み出す。（これが、次でいうところのpreviousHashBlockになるのかな？）
